a58e66aa7536b36d368835d9cc09fcb3e62832fc
Yaroslav Aleksandrov <48127149+tromontana@users.noreply.github.com>
2020-10-28 17:05:03 +0200

    Create recovery.php

---
+++ recovery.php
@@ -0,0 +1,97 @@
+<?php
+require ('config.php');
+if (isset($_POST["recovery"])) {
+    if (!empty($_POST['email'])){
+        $email = addslashes(trim($_POST['email']));
+        $result = $db->prepare('SELECT * FROM userlist WHERE email LIKE :email');
+        $result->execute([':email' => $email]);
+        $count = $result->fetch();
+        if ($count) {
+                $hash = md5($email.time());
+                $subject = "Recovery password for File Manager!";
+                $headers  = "MIME-Version: 1.0\r\n";
+                $headers .= "Content-type: text/html; charset=utf-8\r\n";
+                $headers .= "To: <$email>\r\n";
+                $headers .= "From: <tromomito@gmail.com>\r\n";
+                $message = '
+                <html>
+                <head>
+                <title>Подтвердите Email</title>
+                </head>
+                <body>
+                <p>Что бы восстановить пароль перейдите по <a href="http://filemanager.loc:8888/newpassword.php?hash=' . $hash . '">recovery password</a></p>
+                </body>
+                </html>
+                ';
+                $edit = $db->prepare("UPDATE userlist SET hash = :hash WHERE email = :email");
+                $edit->execute([
+                    ':hash' => $hash,
+                    ':email' => $email,
+                ]);
+                mail($email, $subject, $message, $headers);
+                header("Location: recovery.php");
+                header("Location: recovery.php?msg=Check%20your%20email!");
+                exit;
+
+        }
+        else {
+            header("Location: recovery.php");
+            header("Location: recovery.php?msg=Invalid%20email!%20Try%20another%20one.");
+            exit;
+        }
+
+    }
+    else {
+        header("Location: recovery.php");
+        header("Location: recovery.php?msg=Please%20enter%20your%20email!");
+        exit;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Recovery password File Manager</title>
+    <link href="style.css" media="screen" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+          rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="header">
+</div>
+<?php echo $text; ?>
+<div class="container mrecovery">
+    <div id="recovery">
+        <h1>Recovery password</h1>
+        <br>
+        <hr />
+        <h3 align="center">Enter your email please!</h3>
+        <form action="recovery.php" id="recoveryform" method="post" name="recoveryform">
+            <p><label for="user_email">E-mail<br>
+                    <input class="input" id="email" name="email" size="32" type="email" value=""></label></p>
+            <p class="submit"><input class="button" name="recovery" type="submit" value="Recovery"></p>
+            <p class="regtext">Remember your password?&nbsp;<a href="login.php">Sign in</a>!</p>
+        </form>
+    </div>
+</div>
+<footer class="footer">
+    <ul class="hr">
+        <li>
+            <a href="https://www.instagram.com/tromomito/"><img src="images/instagram.png" width="30"
+                                                                height="30"></a>
+        </li>
+        <li>
+            <a href="https://twitter.com/tromomito"><img src="images/twitter.png" width="30"
+                                                         height="30"></a>
+        </li>
+        <li>
+            <a href="mailto:tromomito@gmail.com"><img src="images/gmail.png" width="30"
+                                                      height="30"></a>
+        </li>
+    </ul>
+    &copy; <?php echo date("Y"); ?> All right reserved!
+</footer>
+</body>
+</html>
