59f7d02a561b9396f55a45039f61ca8a9dd758fc
Yaroslav Aleksandrov <48127149+tromontana@users.noreply.github.com>
2020-11-12 15:11:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,96 @@ if (!isset($_SESSION["sessionUsername"])) {
     header("Location:login.php");
     exit;
 }
-$favor = '';
-function processDir($path)
+
+function processDir($path, $cookie_array)
 {
     $dir_handle = opendir($path);
     while (false !== ($name = readdir($dir_handle))) {
         if ($name == '..') {
             $pathArr = explode('/', $path);
             $pathNoLast = array_slice($pathArr, 0, count($pathArr) - 1);
-            echo '<tr><td></td><td></td><td></td><td>[dir]<a href="index.php?path=' . urlencode(implode('/', $pathNoLast)) . '">' . $name . '</a></td><td></td><td></td><td></td><td></td><td></td></tr>';
+            echo '
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <a href="index.php?path=' . urlencode(implode('/', $pathNoLast)) . '">' . $name . '</a>
+    </td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>';
             continue;
         } elseif ($name == '.') {
             continue;
         }
         if (is_dir($path . '/' . $name)) {
             $nextPath = $path . '/' . $name;
-            echo '<tr><td></td><td></td><td><a href="index.php?delPath=' . urlencode($nextPath) . '" class="delete_btn" onclick="return confirm(\'Are you sure?\')">DELETE</a></td><td>[dir] <a href="index.php?path=' . urlencode
-                ($nextPath) . '">' . $name . '</a></td><td><a href="index.php?addToFavor=' . urlencode($nextPath) . '"><img src="images/add_favourite.png" class="imgFav"></a></td><td>Folder</td><td></td><td></td><td></td></tr>';
+            echo '
+<tr>
+    <td></td>
+    <td>
+        <a href="index.php?delPath=' . urlencode($nextPath) . '" class="btn btn-sm btn-danger" onclick="return confirm(\'Are you sure?\')"><i class="glyphicon glyphicon-trash"></i> DELETE</a>
+    </td>
+    <td>
+        <a href="index.php?path=' . urlencode($path) . '&renamePath=' . urlencode($nextPath) . '" class="btn btn-sm 
+            btn-primary"><i class="glyphicon glyphicon-retweet"></i> RENAME</a>
+    </td>
+    <td>
+        <a href="index.php?path=' . urlencode
+                ($nextPath) . '">' . $name . '</a>
+    </td>
+    <td>
+        <a href="index.php?addToFavor=' . urlencode($nextPath) . '">
+            <img src="images/' . (in_array($nextPath, $cookie_array) ? 'star.jpg' : 'add_favourite.png') . '" class="imgFav"></a>
+    </td>
+    <td>Folder</td>
+    <td></td>
+    <td></td>
+    <td></td>
+</tr>';
         } elseif (is_file($path . '/' . $name)) {
             $filePath = $path . '/' . $name;
             $stat = stat($filePath);
-            echo '<tr bgcolor="#efefef"><td></td><td><a href="index.php?downPath=' . urlencode
-                ($filePath) . '" class="downloadBtn" onclick="return confirm(\'Are you sure you want to download this file?\')">DOWNLOAD</a></td><td><a href="index.php?delPath=' . urlencode($filePath) . '" class="delete_btn" onclick="return confirm(\'Are you sure?\')">DELETE</a></td><td>[file]<a href="' . urlencode
-                ($filePath) . '">' . $name . '</a></td><td><a href="index.php?addToFavor=' . urlencode($filePath) . '"><img src="images/add_favourite.png" class="imgFav"></a></td><td>' . ($stat['size']) . ' b' . '</td><td>' . $stat['gid'] .
-                '</td><td>' . date('d.m.Y', $stat['mtime']) . '</td><td>' . $stat['mode'] . '</td></tr>';
+            $size = $stat['size'];
+            if ($size > 1024 * 1024 * 1024) {
+                $size = round($size / (1024 * 1024 * 1024), 2) . ' Gb';
+            } elseif ($size > 1024 * 1024) {
+                $size = round($size / (1024 * 1024), 2) . ' Mb';
+            } elseif ($size > 1024) {
+                $size = round($size / (1024), 2) . ' Kb';
+            } else {
+                $size = $size / 1 . ' b';
+            }
+            echo '
+<tr bgcolor="#efefef">
+    <td>
+        <a href="index.php?downPath=' . urlencode
+                ($filePath) . '" class="btn btn-sm btn-success" onclick="return confirm(\'Are you sure you want to download this file?\')">
+        <i class="glyphicon glyphicon-save-file"></i> DOWNLOAD</a>
+     </td>
+    <td>
+        <a href="index.php?delPath=' . urlencode($filePath) . '" class="btn btn-sm btn-danger" onclick="return confirm(\'Are you sure?\')"><i class="glyphicon glyphicon-trash"></i> DELETE</a>
+    </td>
+    <td>
+        <a href="index.php?path=' . urlencode($path) . '&renamePath=' . urlencode($filePath) . '" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-retweet"></i> RENAME</a>
+    </td>
+    <td>
+        <a href="' . urlencode
+                ($filePath) . '">' . $name . '</a>
+    </td>
+    <td>
+        <a href="index.php?addToFavor=' . urlencode($filePath) . '">
+        <img src="images/' . (in_array($filePath, $cookie_array) ? 'star.jpg' : 'add_favourite.png') . '" class="imgFav"></a>
+        </td>
+    <td>' . $size . '</td>
+    <td>' . $stat['gid'] . '</td>
+    <td>' . date('d.m.Y', $stat['mtime']) . '</td>
+    <td>' . $stat['mode'] . '</td>
+</tr>';
         }
     }
     closedir($dir_handle);
@@ -39,6 +105,9 @@ $curPath = isset($_GET['path']) ? urldecode($_GET['path']) : getcwd();
 $delPath = isset($_GET['delPath']) ? urldecode($_GET['delPath']) : false;
 $downPath = isset($_GET['downPath']) ? urldecode($_GET['downPath']) : false;
 $addToFavor = isset($_GET['addToFavor']) ? urldecode($_GET['addToFavor']) : false;
+$renamePath = isset($_GET['renamePath']) ? urldecode($_GET['renamePath']) : false;
+
+
 
 if ($delPath) {
     if (is_dir($delPath)) {
@@ -49,6 +118,7 @@ if ($delPath) {
     $pathArr = explode('/', $delPath);
     $pathNoLast = array_slice($pathArr, 0, count($pathArr) - 1);
     header("Location: index.php?path=" . urlencode(implode('/', $pathNoLast)));
+    exit;
 }
 
 if ($downPath) {
@@ -85,20 +155,48 @@ if (isset($_POST['uploadedFileBtn'])) {
 
 if (isset($_SESSION["sessionUsername"])) :
     $text2 .= '
-            <h4 style="float: right" align="right">Welcome ' . $_SESSION['sessionUsername'] . '!</h4>
-            <p align="right"><a href="logout.php"><img src="images/signout.png" width="30"
-                                                       height="30"></a></p>';
+            <h4>Welcome ' . $_SESSION['sessionUsername'] . '! <a href="logout.php"><img src="images/signout.png" width="40" height="40"></a></h4>';
 
 endif;
 
-
-if ($addToFavor) {
-    setcookie("favorites", serialize($addToFavor), time()+3600);
-    var_dump($_COOKIE);exit;
-    if (isset($_COOKIE['favorites'])) {
-    $favor .= '<img src="images/star.jpg" class="imgFav">';
+if ($renamePath) {
+    $script = '$("#renameModal").modal("show");';
+    if (isset($_POST['new_name'])) {
+        $newName = trim($_POST['new_name']);
+        $pathArrRen = explode('/', $renamePath);
+        array_pop($pathArrRen);
+        array_push($pathArrRen, $newName);
+        $newArrRen = implode('/',$pathArrRen);
+        $newRename = rename($renamePath, $newArrRen);
+        $pathArr = explode('/', $renamePath);
+        $pathNoLast = array_slice($pathArr, 0, count($pathArr) - 1);
+        header("Location: index.php?path=" . urlencode(implode('/', $pathNoLast)));
+        exit;
     }
 }
+
+
+$cookie_array = unserialize($_COOKIE['favorites']);
+
+if ($addToFavor) {
+    $removed = false;
+    foreach ($cookie_array as $k => $value) {
+        if ($value == $addToFavor) {
+            unset($cookie_array[$k]);
+            $removed = true;
+        }
+    }
+    if (!$removed) {
+        $cookie_array[] = $addToFavor;
+    }
+    setcookie('favorites', serialize($cookie_array), time() + 3600);
+    $pathArr = explode('/', $addToFavor);
+    $pathNoLast = array_slice($pathArr, 0, count($pathArr) - 1);
+    header("Location: index.php?path=" . urlencode(implode('/', $pathNoLast)));
+    exit;
+}
+$cookie_array = unserialize($_COOKIE['favorites']);
+
 ?>
 
 <!doctype html>
@@ -106,35 +204,36 @@ if ($addToFavor) {
 <head>
     <meta charset="utf-8">
     <title>File manager</title>
-    <link rel="icon" type="image/png" sizes="18x18" href="images/cherep.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="style.css" media="screen" rel="stylesheet">
 </head>
 <body>
 <div class="header">
-    <?php echo $text; ?>
+    <div class="in_line">
+    <h4><a href="http://filemanager.loc:8888"><img src="images/home.png" width="50" height="50"></a>File
+        manager</h4>
+        <?php echo $text2; ?>
+    </div>
 </div>
 <div class="container mindex">
-    <hr />
+    <?php echo $text; ?>
     <form method="post" enctype="multipart/form-data">
-    <div style="clear: both">
-            <a href="http://filemanager.loc:8888"><img src="images/home.png" width="50" height="50"></a>
-        <?php echo $text2; ?>
-        </div>
-        <hr />
-        <label for="nameFolder">Please enter "Folder name"!</label><br>
+        <h3><span class="label label-default">Please enter "Folder name"!</span></h3><br>
         <input type="text" name="nameFolder">
-        <input class="create_btn" type="submit" value="Create folder"><br>
+        <button class="btn btn-bg btn-primary" type="submit"><i class="glyphicon glyphicon-folder-close"></i> Create
+            folder</button><br>
         <hr />
-        <label for="uploadedFile">Upload your file.</label><br><br>
-        <input type="file" name="uploadedFile">
-        <input class="uploadBtn" type="submit" name="uploadedFileBtn" value="Upload"><br>
+        <h3><span class="label label-default">Upload your file.</span></h3><br>
+        <input type="file" name="uploadedFile"><br>
+        <button class="btn btn-bg btn-primary" type="submit" name="uploadedFileBtn"><i class="glyphicon
+        glyphicon-open"></i> Upload</button><br>
         <hr />
         <br>
         <table class="tableFM">
             <tr>
-                <th>Favorites</th>
                 <th>Download</th>
                 <th>Delete</th>
+                <th>Rename</th>
                 <th>File</th>
                 <th>Add to favorites</th>
                 <th>Size Byte</th>
@@ -142,9 +241,30 @@ if ($addToFavor) {
                 <th>Date</th>
                 <th>Inode</th>
             </tr>
-            <?php processDir($curPath); ?>
+            <?php processDir($curPath, $cookie_array); ?>
         </table>
     </form>
+</div>
+<div class="modal fade bs-example-modal-sm" id="renameModal" tabindex="-1" role="dialog"
+     aria-labelledby="mySmallModalLabel">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Rename file or folder</h5>
+            </div>
+            <div class="modal-body">
+                <form method="post">
+                    <div class="form-group">
+                    New name: <input type="text" name="new_name">
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Rename</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
  <footer class="footer">
             <ul class="hr">
@@ -162,6 +282,16 @@ if ($addToFavor) {
                 </li>
             </ul>
             &copy; <?php echo date("Y"); ?> All right reserved!
-        </footer>
+ </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+    <?= $script ?>
+</script>
+
 </body>
 </html>
