21e2fb1d20ababe13281206e5a4801c28ce42557
Yaroslav Aleksandrov <48127149+tromontana@users.noreply.github.com>
2020-11-12 10:56:02 +0200

    Update register.php

--- register.php
+++ register.php
@@ -40,15 +40,14 @@ if (isset($_POST['register'])) {
 <head>
     <meta charset="utf-8">
     <title>Registration File Manager</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="style.css" media="screen" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-          rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div class="header">
 </div>
-<?php echo $text; ?>
 <div class="container mregister">
+    <?php echo $text; ?>
     <div id="login">
         <h1>Registration</h1>
         <form action="register.php" id="registerform" method="post" name="registerform">
@@ -60,8 +59,8 @@ if (isset($_POST['register'])) {
                     <input class="input" id="username" name="username" size="20" type="text" value=""></label></p>
             <p><label for="user_pass">Password<br>
                     <input class="input" id="password" name="password" size="32" type="password" value=""></label></p>
-            <p class="submit"><input class="button" id="register" name="register" type="submit"
-                                     value="Register now"></p>
+            <button class="btn btn-bg btn-success" id="register" name="register" type="submit"><i class="glyphicon
+                        glyphicon-user"></i> Register now</button>
             <p class="regtext">Already Registered?&nbsp;<a href="login.php">Enter username please</a>!</p>
         </form>
     </div>
